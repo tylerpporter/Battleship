90231eeea25a3eeeb12248244f9a7bc59eeb6acb
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-05 16:20:28 -0700

    Passed all ship tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,9 +2,23 @@ require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/ship'
 
-  class ShipTest < Minitest::Test
+class ShipTest < Minitest::Test
 
   def setup
     @cruiser = Ship.new("Cruiser", 3)
   end
+
+  def test_when_hit_loses_one_health
+    @cruiser.hit
+
+    assert_equal 2, @cruiser.health
   end
+
+  def test_if_sunk
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+
+    assert @cruiser.sunk?
+  end
+end
