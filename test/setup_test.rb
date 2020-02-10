3cc061a2ba1d5c76d4c6bf7c00bc8d36ca27ce31
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-10 16:30:32 -0700

    refactored tests

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -2,17 +2,20 @@ require_relative 'test_helper.rb'
 require './lib/setup.rb'
 
 class SetupTest < Minitest::Test
+def setup
+  @setup = Setup.new("p")
 
+end
   def test_it_exists
 
-  assert_instance_of Setup, Setup.new("p")
+  assert_instance_of Setup, @setup
   end
 
   def test_if_it_stores_attributes
 
-    assert_equal Board, Setup.new("p").computer_board.class
-    assert_equal Ship, Setup.new("p").computer_ships.first.class
-    assert_equal Array, Setup.new("p").player_ships.class
+    assert_equal Board, @setup.computer_board.class
+    assert_equal Ship, @setup.computer_ships.first.class
+    assert_equal Array, @setup.player_ships.class
   end
 
 end
