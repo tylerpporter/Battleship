054514c80cea860cfab31b36c15b47077d410387
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-10 16:00:49 -0700

    passed user decision test

--- test/main_menu_test.rb
+++ test/main_menu_test.rb
@@ -5,7 +5,13 @@ class MenuTest < Minitest::Test
 
 
   def test_if_it_exists
+
     assert_instance_of Menu, Menu.new
+  end
+
+  def test_stores_user_decision
+
+    assert_equal "p", Menu.new.user_decision
   end
 
 end
