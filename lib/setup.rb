66a8496f6c1c901c11bf9a3471635df99dca7342
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-10 16:29:05 -0700

    added computer and player attributes

--- lib/setup.rb
+++ lib/setup.rb
@@ -1,2 +1,28 @@
+require './lib/main_menu.rb'
+require './lib/board.rb'
+require './lib/ship.rb'
+
 class Setup
+attr_reader :computer_board,
+            :player_board,
+            :computer_ships,
+            :player_ships
+
+  def initialize(user_decision)
+    if user_decision == "p"
+      @computer_board = Board.new
+      @player_board = Board.new
+      computer_ship1 = Ship.new("Submarine", 2)
+      computer_ship2 = Ship.new("Cruiser", 3)
+      player_ship1 = Ship.new("Submarine", 2)
+      player_ship2 = Ship.new("Cruiser", 3)
+      @computer_ships = [computer_ship1, computer_ship2]
+      @player_ships = [player_ship1, player_ship2]
+    end
+
+    def place_computer_ships
+
+    end
+  end
+
 end
--- test/setup_test.rb
+++ test/setup_test.rb
@@ -2,10 +2,17 @@ require_relative 'test_helper.rb'
 require './lib/setup.rb'
 
 class SetupTest < Minitest::Test
-  
+
   def test_it_exists
 
-  assert_instance_of Setup, Setup.new
+  assert_instance_of Setup, Setup.new("p")
+  end
+
+  def test_if_it_stores_attributes
+
+    assert_equal Board, Setup.new("p").computer_board.class
+    assert_equal Ship, Setup.new("p").computer_ships.first.class
+    assert_equal Array, Setup.new("p").player_ships.class
   end
 
 end
