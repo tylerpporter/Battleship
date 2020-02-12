adfdb23dfb0e3cc8b8def96e9940dcfb728a995e
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-12 15:12:07 -0700

    updated main menu

--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -1,12 +1,14 @@
 class Menu
   attr_reader :user_decision
   def start
-    msg1 = "Welcome".bold.red
-    msg2 = "to".bold.white
-    msg3 = "BATTLESHIP!".bold.blue
-    msg4 = "-------".red
-    msg5 = "----".white
-    msg6 = "-----------".blue
+    banner0 = "██████╗  █████╗ ████████╗████████╗██╗     ███████╗███████╗██╗  ██╗██╗██████╗ ".red
+    banner1 = "██╔══██╗██╔══██╗╚══██╔══╝╚══██╔══╝██║     ██╔════╝██╔════╝██║  ██║██║██╔══██╗".white
+    banner2 = "██████╔╝███████║   ██║      ██║   ██║     █████╗  ███████╗███████║██║██████╔╝".blue
+    banner3 = "██╔══██╗██╔══██║   ██║      ██║   ██║     ██╔══╝  ╚════██║██╔══██║██║██╔═══╝ ".red
+    banner4 = "██████╔╝██║  ██║   ██║      ██║   ███████╗███████╗███████║██║  ██║██║██║     ".white
+    banner5 = "╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝     ".blue
+    banner7 = "███████╗███████╗███████╗███████╗███████╗███████╗███████╗███████╗███████╗███████╗".white
+    banner8 = "╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝".white
     msg7 = "Enter".red
     msg8 = "p".bold.red
     msg9 = "to".white
@@ -14,8 +16,14 @@ class Menu
     msg11 = "q".bold.blue
     msg12 = "to quit.".blue
     puts ""
-    puts "#{msg1} #{msg2} #{msg3}"
-    puts "#{msg4}#{msg5}#{msg6}"
+    puts "#{banner0}"
+    puts "#{banner1}"
+    puts "#{banner2}"
+    puts "#{banner3}"
+    puts "#{banner4}"
+    puts "#{banner5}"
+    puts "#{banner7}"
+    puts "#{banner8}"
     puts "#{msg7} #{msg8} #{msg9} #{msg10} #{msg11} #{msg12}"
     puts ""
     @user_decision = gets.chomp.downcase
